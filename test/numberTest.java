f3f319512888721c8bab8ba1d9ad55c6b7271a7d
Jeferson Tapias <jtapiase@unal.edu.co>
2018-05-19 01:29:34 -0500

    adding another method for in unit test class

--- test/numberTest.java
+++ test/numberTest.java
@@ -33,7 +33,19 @@ public class numberTest {
         /*we assert that a random multiple of 5 number from the list, is checked by the method and get 
         the expected message*/
         assertEquals("Buzz", number.multiple5(numberList.get(19)));
-    }   
+    }
+    
+    //testing that a number that is multiple of both, prints 'FizzBuzz'
+    @Test
+    public void multiple5or3Test(){
+        //given a number type variable, we get the default numer list created in the object
+        number number = new number();
+        List<Integer> numberList = number.getNumber();
+        
+        /*we assert that a random multiple of 5 number from the list, is checked by the method and get 
+        the expected message*/
+        assertEquals("FizzBuzz", number.multiple3(numberList.get(14))+number.multiple5(numberList.get(14)));
+    }  
     
     /*for numbers that are not multiples of 3 or 5, we take for granted that systems prints them correctly
     due to this is a native method*/
